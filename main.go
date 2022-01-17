e4f15094f9452d07fbacde261bdea8c39794dc49
pvv <pvv@equifax.local>
2022-01-17 11:55:12 +0300

    add new tmp

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"io"
+	"github.com/pkg/errors"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -14,13 +15,15 @@ var (
 		"conn", "PostgreSQL connection string in URL format").Required().String()
 	schema = kingpin.Flag(
 		"schema", "PostgreSQL schema name").Default("public").Short('s').String()
-	pkgName          = kingpin.Flag("package", "package name").Default("main").Short('p').String()
-	typeMapFilePath  = kingpin.Flag("typemap", "column type and go type map file path").Short('t').String()
-	exTbls           = kingpin.Flag("exclude", "table names to exclude").Short('x').Strings()
-	customTmpl       = kingpin.Flag("template", "custom template path").String()
-	outFile          = kingpin.Flag("output", "output file path").Short('o').String()
-	noQueryInterface = kingpin.Flag("no-interface", "output without Queryer interface").Bool()
+	pkgName         = kingpin.Flag("package", "package name").Default("main").Short('p').String()
+	typeMapFilePath = kingpin.Flag("typemap", "column type and go type map file path").Short('t').String()
+	exTbls          = kingpin.Flag("exclude", "table names to exclude").Short('x').Strings()
+	customTmpl      = kingpin.Flag("template", "custom template path").Default("template/methods.gohtml").String()
+	outPath         = kingpin.Flag("output", "output directory path").Short('o').String()
+	onlyInterface   = kingpin.Flag("only-interface", "output without Criteria and order types").Short('i').Bool()
 )
+
+//go:generate go get golang.org/x/tools/cmd/goimports
 
 func main() {
 	kingpin.Parse()
@@ -30,35 +33,73 @@ func main() {
 		log.Fatal(err)
 	}
 
-	st, err := PgCreateStruct(conn, *schema, *typeMapFilePath, *pkgName, *customTmpl, *exTbls)
+	oInfo, err := os.Stat(*outPath)
+	if !oInfo.IsDir() {
+		log.Fatalln("Out path is not a directory")
+	}
+
+	builder := NewPgOrmBuilder(conn, *schema, *typeMapFilePath, *pkgName, *exTbls)
+
+	tables, err := builder.GetPgStruct()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var src []byte
-	if *noQueryInterface {
-		src = st
-	} else {
-		q := []byte(queryInterface)
-		src = append(st, q...)
-	}
-
-	var out io.Writer
-	if *outFile != "" {
-		out, err = os.Create(*outFile)
-		if err != nil {
-			log.Fatalf("failed to create output file %s: %s", *outFile, err)
-		}
-	} else {
-		out = os.Stdout
-	}
-	if _, err := out.Write(src); err != nil {
+	outPutPath := path.Join(*outPath, *schema)
+	err = os.MkdirAll(outPutPath, 0775)
+	if err != nil {
 		log.Fatal(err)
 	}
-	if *outFile != "" {
-		params := []string{"-w", *outFile}
-		if err := exec.Command("goimports", params...).Run(); err != nil {
-			log.Fatalf("failed to goimports: %s", err)
+
+	src, err := builder.RenderPgCustomTmpl(nil, "template/queryInterface.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = writeFile(outPutPath, "queryInterface", src)
+	if err != nil {
+		log.Fatalf("failed to create output file %s: %s", "queryInterface", err)
+	}
+
+	if !*onlyInterface {
+		src, err := builder.RenderPgCustomTmpl(nil, "template/queryTypes.gohtml")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = writeFile(outPutPath, "queryTypes", src)
+		if err != nil {
+			log.Fatalf("failed to create output file %s: %s", "queryTypes", err)
 		}
 	}
+
+	for _, tbl := range tables {
+		src, err := builder.RenderPgCustomTmpl(tbl, *customTmpl)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = writeFile(outPutPath, tbl.Name, src)
+		if err != nil {
+			log.Fatalf("failed to create output file %s: %s", tbl.Name, err)
+		}
+	}
+}
+
+func writeFile(outPutPath string, tblName string, src []byte) (err error) {
+
+	outFile := path.Join(outPutPath, tblName+".go")
+
+	out, err := os.Create(outFile)
+	if err != nil {
+		return errors.Wrap(err, "failed to create output file: ")
+	}
+	if _, err := out.Write(src); err != nil {
+		return err
+	}
+	params := []string{"-w", outFile}
+	if err = exec.Command("goimports", params...).Run(); err != nil {
+		err = errors.Wrap(err, "failed to goimports: ")
+	}
+	return err
 }
